c92ca1fe3d9b6921ecf3ac57d8ac87da05255c1e
AzizSaidrakhmonov <saidrakhmonovaziz@gmail.com>
2022-04-13 13:43:20 +0500

    wensite

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+function init() {
+  setTimeout(() => {
+    $("#loader").fadeToggle(1500);
+  }, 2000);
+}
+
+init();
+
+
---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+const typed = new Typed(".typing", {
+    strings: [
+        "Hello",
+        "My name is Azizxo'ja Saidrahmonov.",
+        "I am a Frontend Developer.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 3000,
+    
+  });
+
+
+
+  
